33e560489e878b0d699499d3ce90c882d50071d6
magriker <magken7@gmail.com>
2024-12-23 23:09:14 +0900

    commit

--- crud/models.py
+++ crud/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
 
 class Product(models.Model):
@@ -8,3 +8,7 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        return reverse('list')
+    
